b5b8f74535da1031defdd4559b6b3e2280c0cc5c
fluxtransistor <lukipav@gmail.com>
2021-03-30 20:54:29 +0200

    Implement Pickle state saving

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import yaml
+import pickle
 
 with open("config.yml", "r") as yml_file:
     config = yaml.load(yml_file)
 
+state = None
+
+
+def save_state():
+    try:
+        with open("data.pickle", "wb") as f:
+            pickle.dump(state, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error saving committee state:", ex)
